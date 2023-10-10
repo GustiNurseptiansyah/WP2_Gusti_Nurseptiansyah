687d728ec9fbd075a4800283d66a6d19d9db7acc
Gusti <gusti9pp@gmail.com>
2023-10-11 00:37:06 +0700

    Menambahkan isi file-file

---
+++ application/views/Home.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>APASIH</title>
+    <style>
+        .rrr{
+            margin: 20px auto;
+            padding: 20px;
+            width: 500px;
+            background: #D2691E;
+            color: white;
+            border-radius: 20px;
+        }
+        h1{
+            text-align: center;
+            font-size: 50px;
+            color: white;
+        }
+        .eee{
+            font-size: 20px;
+            text-align: center;
+        }
+        body{
+            background: #FAEBD7;
+            background-image: url("foto1.png");
+            background-size: cover;
+        }
+        img{
+            width: 200px;
+            height: 200px;
+        }
+    </style>
+</head>
+<body>
+    <h1>welcome</h1>
+    <hr>
+    <div class="rrr">
+        <?php
+        echo '<p class="eee">Perkenalan</p>';
+        echo "<p>Nama : Gusti Nurseptiansyah</p>";
+        echo "<p>Nim : 12220332</p>";
+        echo "<p>Saya adalah seorang pemula ajarin dong puh puh sepuhhh tingky wingky dipsy lala puh sepuhhh</p>";
+        ?>
+    </div>
+    <div class="rrr">
+        <?php
+        echo '<p class="eee">kareer</p>';
+        echo "<p>Game Developer</p>";
+        echo "<p>Web Developer</p>";
+        echo "<p>UI & UX Designer</p>";
+        ?>
+    </div>
+    <div class="rrr">
+        <?php
+        echo '<p class="eee">Pendidikan</p>';
+        echo "<p>SMAN 1 Tamansari</p>";
+        echo "<p>SMPN 3 Ciomas</p>";
+        echo "<p>SDN Sukamantri 03</p>";
+        ?>
+    </div>
+</body>
+</html>
